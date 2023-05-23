42a2e36246389c80553533e17037858b9ada9353
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2023-05-23 15:29:15 +0200

    Describe outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
 output "external_ingress_url" {
-  value = var.ingress_host == null ? null : "https://${var.ingress_host}"
+  description = "Public URL for connecting to deployed Kratos instance from outside the cluster, if ingress_host was provided"
+  value       = var.ingress_host == null ? null : "https://${var.ingress_host}"
 }
 
 output "internal_service_url" {
+  description = "Cluster-private URLs for connecting to deployed Kratos instance, both public and admin API endpoints"
   value = {
     public = local.service_url
     admin  = "${local.service_url}:4434"
@@ -10,5 +12,6 @@ output "internal_service_url" {
 }
 
 output "service_name" {
-  value = kubernetes_service_v1.kratos_service.metadata[0].name
+  description = "Name of created Kubernetes service for use with other routing schemes"
+  value       = kubernetes_service_v1.kratos_service.metadata[0].name
 }
